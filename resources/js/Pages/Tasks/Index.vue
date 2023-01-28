d7d0a48d474e937566a9fa0de7ce57f63e0d3848
H3yd4y <OxideO2CO2@gmail.com>
2023-01-28 15:38:39 +0800

    first commit

---
+++ resources/js/Pages/Tasks/Index.vue
@@ -0,0 +1,33 @@
+<template>
+<AppLayout title ="Task Management">
+ <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Task Management
+            </h2>
+            <PrimaryButton @click = "createTask">
+                Create Task
+            </PrimaryButton>
+        </template>
+        
+
+
+</AppLayout>
+</template>
+
+<script>
+import AppLayout from "@/Layouts/Applayout.vue";
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import {router} from '@inertiajs/vue3'
+
+export default {
+    
+    components: {
+        AppLayout, PrimaryButton
+    },
+    methods: {
+        createTask(){
+            router.visit(route('tasks.create'),{method: 'get'})
+        }
+    }
+}
+</script>
