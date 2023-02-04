f9c92cdd9e0cb71ca965ee0e45e9e63bf4504837
H3yd4y <OxideO2CO2@gmail.com>
2023-02-04 11:30:40 +0800

    Edit/Delete

---
+++ resources/js/Pages/Tasks/Edit.vue
@@ -0,0 +1,111 @@
+<template>
+    <AppLayout title = "Create Task">
+        
+    <FormSection>
+        <template #title>
+            Task Information
+        </template>
+
+        <template #description>
+            huhu
+        </template>
+
+        <template #form>
+            
+            <!-- Name -->
+            <div class="col-span-6 sm:col-span-4">
+                <InputLabel for="description" value="Description:" />
+                <TextInput
+                    id="description"
+                    v-model="form.description"
+                    type="text"
+                    class="mt-1 block w-full"
+                    autocomplete="description"
+                />
+                <InputError :message="form.errors.description" class="mt-2" />
+            </div>
+
+            <!-- Email -->
+            <div class="col-span-6 sm:col-span-4">
+                <InputLabel for="status" value="Status:" />
+                <TextInput
+                    id="status"
+                    v-model="form.status"
+                    type="text"
+                    class="mt-1 block w-full"
+                    autocomplete="status"
+                />
+                <InputError :message="form.errors.status" class="mt-2" />
+            </div>
+
+            <div class="col-span-6 sm:col-span-4">
+                <InputLabel for="assignee" value="Assignee:" />
+                <TextInput
+                    id="assignee"
+                    v-model="form.assignee"
+                    type="text"
+                    class="mt-1 block w-full"
+                    autocomplete="assignee"
+                />
+                <InputError :message="form.errors.assignee" class="mt-2" />
+            </div>
+        </template>
+
+        <template #actions>
+            <ActionMessage :on="form.recentlySuccessful" class="mr-3">
+                Saved.
+            </ActionMessage>
+
+            <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing" @click = 'saveTask'>
+                Save Lods
+            </PrimaryButton>
+        </template>
+    </FormSection>
+
+    </AppLayout>
+</template>
+<script>
+import AppLayout from "@/Layouts/Applayout.vue";
+import { Link } from '@inertiajs/vue3';
+import ActionMessage from '@/Components/ActionMessage.vue';
+import FormSection from '@/Components/FormSection.vue';
+import InputError from '@/Components/InputError.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import SecondaryButton from '@/Components/SecondaryButton.vue';
+import TextInput from '@/Components/TextInput.vue';
+export default {
+    props:{
+        task: Object
+    },
+    components: {
+        AppLayout,
+        Link,
+        ActionMessage,
+        FormSection,
+        InputError,
+        InputLabel,
+        PrimaryButton,
+        SecondaryButton,
+        TextInput
+    },
+
+data: function(){
+    return{
+    form: this.$inertia.form({
+        description: this.task.description,
+        status: this.task.status_string,
+        assignee: this.task.assignee
+    }),
+};
+},
+methods: {
+    saveTask(){
+        this.$inertia.put(route('tasks.update', this.task.id), this.form,{
+            onSuccess: () => this.form.reset()
+        });
+    }
+}
+}
+
+</script>
--- resources/js/Pages/Tasks/Index.vue
+++ resources/js/Pages/Tasks/Index.vue
@@ -1,32 +1,88 @@
 <template>
 <AppLayout title ="Task Management">
  <template #header>
+            
+            <PrimaryButton class="float-right" @click = "createTask">
+                Create Task
+            </PrimaryButton>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 Task Management
             </h2>
-            <PrimaryButton @click = "createTask">
-                Create Task
-            </PrimaryButton>
+          
         </template>
-        
+        <table class="table-auto">
+  <thead>
+    <tr class = bg-green-500 text: white>
+      <th>ID</th>
+      <th>DESCRIPTION</th>
+      <th>STATUS</th>
+      <th>ASSIGNEE</th>
+      <th>ACTION</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr class="text-center odd:bg-white even:bg-slate-200 hover:bg-blue-500 hover:text-white" v-for="(task,index) in collection.tasks" :key="Index">
+      <td>{{ task.id }}</td>
+      <td>{{ task.description }}</td>
+      <td>{{ task.status_string }}</td>
+      <td>{{ task.assignee }}</td>
+      <td>
+        <div class="flex gap-2 justify-center">
+            <SecondaryButton @click="editTask(task.id)">
+                Edit
+            </SecondaryButton >
+            <DangerButton @click="showDeleteDialog">
+                Delete
+            </DangerButton>
+           
 
+        </div>
+      </td>
+    </tr>
 
+  </tbody>
+</table>
+
+<!-- Delete Confirmation Dialog -->
+<DialogModal :show="show_delete_dialog" @close="show_delete_dialog = false">
+    <template #title>
+        <h1>Delete Task</h1>
+    </template>
+</DialogModal>
 </AppLayout>
 </template>
 
 <script>
 import AppLayout from "@/Layouts/Applayout.vue";
 import PrimaryButton from '@/Components/PrimaryButton.vue';
+import SecondaryButton from '@/Components/SecondaryButton.vue';
+import DangerButton from '@/Components/DangerButton.vue';
+import DialogModal from '@/Components/DialogModal.vue';
 import {router} from '@inertiajs/vue3'
 
 export default {
     
     components: {
-        AppLayout, PrimaryButton
+        AppLayout, PrimaryButton, SecondaryButton, DangerButton, DialogModal
+    },
+    data: function(){
+        return{
+            show_delete_dialog:false
+        }
+        
+    },
+    props: {
+        collection:Object
     },
     methods: {
         createTask(){
             router.visit(route('tasks.create'),{method: 'get'})
+        },
+        editTask(id){
+            this.$inertia.visit(route('tasks.edit', id), {method:'get'})
+        },
+        showDeleteDialog(){
+            this.show_delete_dialog = true;
         }
     }
 }
